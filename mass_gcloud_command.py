69ae3e03973dd4c2257a1bb7e00d134fd8d50475
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-04-09 19:45:59 -0400

    Zone-specific GPU quotas

--- mass_gcloud_command.py
+++ mass_gcloud_command.py
@@ -13,14 +13,21 @@ GIT_PULL_CMD = "cd Oceananigans.jl/; git pull; git checkout as/fc"
 JL_ACTIVATE_CMD = "cd Oceananigans.jl/; /opt/julia/julia-1.1.0/bin/julia --project -e \"using Pkg; Pkg.activate(\".\"); Pkg.instantiate(); Pkg.build();\""
 FREE_CONVECTION_CMD = "cd Oceananigans.jl/; nohup /opt/julia/julia-1.1.0/bin/julia --project examples/free_convection_mod.jl 75 0.01 128 6 8 1e-4 </dev/null >foo"
 
-zones = ["us-central1-b", "us-west1-b", "us-east1-b", "us-east4-b", "us-west2-b"]
+zones = ["us-west1-b", "us-central1-b", "asia-east1-c", "europe-west4-c"]
+
+gpu_quotas = {
+        "us-west1-b": 16,
+        "us-central1-b": 4,
+        "asia-east1-c": 1,
+        "europe-west4-c": 1
+        }
 
 
 def spin_up_instances(name, username):
     n = 0
     instances = []
     for zone in zones:
-        for _ in range(GPU_QUOTA):
+        for _ in range(gpu_quotas[zone]):
             n = n + 1
             instance_name = name + str(n) 
             logger.info("Spinning up instance {:s} on zone {:s}...".format(instance_name, zone))
@@ -40,7 +47,7 @@ def spin_up_instances(name, username):
     return instances
 
 
-def delete_instances(instances, name, username):
+def delete_instances(instances, username):
     for instance in instances:
         delete_cmd = "gcloud --verbosity=info --quiet compute instances delete " + \
                      instance["name"] + " --zone " + instance["zone"] + " --delete-disks all"
@@ -67,4 +74,5 @@ def run_mass_gcloud_command(instances, username, command):
 if __name__ == "__main__":
     username = "alir"
     instances = spin_up_instances(name="convection", username="alir")
-    run_mass_gcloud_command(instances, username, PLUS_CMD)
+    delete_instances(instances, username)
+    # run_mass_gcloud_command(instances, username, PLUS_CMD)
